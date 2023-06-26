c728fd9ddafd948cc6c87b63725cffe3df1fbc76
Daniel Bryte <danielbryte1@gmail.com>
2023-06-26 17:53:43 +0100

    feat: the social element and navbar section

---
+++ components/Socials.js
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+const Socials = () => {
+    return (
+        <main className='cta'>
+            <div className="mail">
+                <p>Mail</p>
+                <Link href="mailto:ezedinirudaniel@gmail.com"><p>ezedinirudaniel@gmail.com</p>
+                </Link>
+            </div>
+
+            <div className="social">
+                <p>Follow</p>
+                <a href="https://www.linkedin.com/in/danielbryte/">
+                    <Image src="/socials/linkedin.svg" width="25" height="25" />
+                </a>
+                <a href="https://twitter.com/amdanielbryte">
+                    <Image src="/socials/twitter.svg" width="25" height="25" />
+                </a>
+                <a href="https://www.instagram.com/amdanielbryte/">
+                    <Image src="/socials/instagram.svg" width="25" height="25" />
+                </a>
+                <a href="https://github.com/DanielBryte">
+                    <Image src="/socials/github.svg" width="25" height="25" />
+                </a>
+            </div>
+        </main>
+    )
+}
+
+export default Socials
