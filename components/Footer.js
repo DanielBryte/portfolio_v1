df3b4d59861070c9b953bc2556b9afd5e4d1c793
Daniel Bryte <danielbryte1@gmail.com>
2023-06-26 17:50:53 +0100

    feat: build the footer section

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h2>© 2023. All rights reserved by Daniel Ezediniru.</h2>
+        </div>
+    )
+}
+
+export default Footer
