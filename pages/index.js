bc86e59dea46d4a65587692626a3416041ac394b
Daniel Bryte <danielbryte1@gmail.com>
2023-07-27 15:28:13 +0100

    feat: implement support feature

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,25 @@ import HomeImg from "/public/home-img.png";
 import Image from "next/image";
 import Link from "next/link";
 
+const Support = () => {
+  return (
+    <div className="support-div">
+      <p className="support-note">Want to support? Buy me a coffee</p>
+      <div className="support-wrapper">
+        <a className="support" href="https://www.buymeacoffee.com/danielbryte" target="_blank"><img src="https://cdn.buymeacoffee.com/widget/assets/coffee%20cup.svg" alt="Buy Me A Coffee"></img></a>
+        <div className="ping"></div>
+      </div>
+    </div>
+  )
+
+}
+
 
 const Home = () => {
-
 
   return (
     <div>
       <PageAnimation />
-
       <main className="body">
         <div className="img-wrap">
           <Image
@@ -19,7 +30,7 @@ const Home = () => {
             width="100%"
             height="100%"
             priority
-            lazy
+            lazy="true"
             placeholder="blur"
             src={HomeImg}
             alt="Ezediniru Daniel"
@@ -53,21 +64,22 @@ const Home = () => {
 
               <div className="mobile-social">
                 <a href="https://www.linkedin.com/in/danielbryte/" target="_blank">
-                  <Image src="/socials/linkedin.svg" width="25" height="25" alt="linkedin" target="_blank" priority/>
+                  <Image src="/socials/linkedin.svg" width="25" height="25" alt="linkedin" target="_blank" priority />
                 </a>
                 <a href="https://twitter.com/amdanielbryte">
-                  <Image src="/socials/twitter.svg" width="25" height="25" alt="Twitter" target="_blank" priority/>
+                  <Image src="/socials/twitter.svg" width="25" height="25" alt="Twitter" target="_blank" priority />
                 </a>
                 <a href="https://www.behance.net/amdanielbryte" target="_blank">
-                    <Image src="/socials/behance.svg" width="25" height="25" alt='Behance' priority />
+                  <Image src="/socials/behance.svg" width="25" height="25" alt='Behance' priority />
                 </a>
                 <a href="https://github.com/DanielBryte">
-                  <Image src="/socials/github.svg" width="25" height="25" alt="Github" target="_blank" priority/>
+                  <Image src="/socials/github.svg" width="25" height="25" alt="Github" target="_blank" priority />
                 </a>
               </div>
             </div>
           </div>
         </motion.section>
+        <Support />
       </main>
     </div>
   )
