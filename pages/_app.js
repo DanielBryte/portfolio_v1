d1ce83b7840b52e5bc1e33fb983e28d836a39c12
Daniel Bryte <danielbryte1@gmail.com>
2023-07-27 16:51:44 +0100

    feat: implement google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Social from "/components/Socials";
 import Nav from "components/Nav";
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 
 
@@ -12,10 +13,25 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <title>Ezediniru Daniel</title>
-        
-				{/* Favicon */}
+
+        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
 
 
