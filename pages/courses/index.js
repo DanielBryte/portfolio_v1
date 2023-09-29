2d4cf4b533dbac582c34603ca459e3f53414beec
Daniel Bryte <danielbryte1@gmail.com>
2023-09-29 09:21:47 +0100

    update website with latest projects

---
+++ pages/courses/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <section className="section">
+    <div className="section-heading">
+      <h1>Digital Courses</h1>
+      <h2>I am sharing my premium professional experience, skills, and knowledge. Making it accesible to all for a one-time fee and some for free.</h2>
+      </div>
+      </section>
+  )
+}
